f79d415a0f3fe6d91c281a05432b52bd990334f8
Alexander T.C <82457422+FluxFeint@users.noreply.github.com>
2023-05-22 09:30:40 +0200

    Update newCat.js

--- src/front/js/pages/newCat.js
+++ src/front/js/pages/newCat.js
@@ -1,88 +1,69 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export const NewCat = () => {
-  const [name, setName] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [alertVariant, setAlertVariant] = useState("");
-  const [alertMessage, setAlertMessage] = useState("");
+export const UserCats = () => {
+  const [cats, setCats] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
   const token = localStorage.getItem("miTokenJWT");
 
   if (!token) {
-    // Mmmmm... no tengo el token, no debería poder acceder a está página de React
-    navigate('/login');
+    navigate("/login");
   }
 
+  useEffect(() => {
+    const fetchUserCats = async () => {
+      try {
+        const response = await fetch(process.env.BACKEND_URL + "/api/cats", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+        if (response.ok) {
+          const data = await response.json();
+          setCats(data);
+        } else {
+          setError("Error al obtener los gatos del usuario");
+        }
+      } catch (error) {
+        setError("Error de conexión");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    // Ejercicio 4
+    fetchUserCats();
+  }, [token]);
 
-    const response = await fetch(process.env.BACKEND_URL + "/api/cat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`,
-      },
-      body: JSON.stringify({ name, imageUrl }),
-    });
+  if (loading) {
+    return <p>Cargando gatos...</p>;
+  }
 
-    const data = await response.json();
-
-    if (response.ok) {
-      setAlertVariant("success");
-      setAlertMessage("Gato añadido correctamente");
-      setName("");
-      setImageUrl("");
-    } else {
-      setAlertVariant("danger");
-      setAlertMessage(data.error || "Error al añadir el gato. Mira la consola o en el terminal del servidor de Python");
-    }
-  };
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
     <div>
-      <div className="container">
-        <h2 className="text-center mt-4 mb-5">Añadir nuevo Gato</h2>
-      </div>
-      {alertMessage && (
-        <div className={`alert alert-${alertVariant}`} role="alert">
-          {alertMessage}
-        </div>
+      <h2>Mis Gatos</h2>
+      {cats.length === 0 ? (
+        <p>No tienes gatos</p>
+      ) : (
+        <ul>
+          {cats.map((cat) => (
+            <li key={cat.id}>
+              <h3>{cat.name}</h3>
+              <img src={cat.image_url} alt={cat.name} />
+            </li>
+          ))}
+        </ul>
       )}
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Nombre del gato
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="imageUrl" className="form-label">
-            URL de la imagen
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="imageUrl"
-            value={imageUrl}
-            onChange={(e) => setImageUrl(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Añadir Gato
-        </button>
-      </form>
     </div>
   );
 };
